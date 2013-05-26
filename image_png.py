bfd0b7e607e7558d060d554acf02b8d69a6ac62b
filletzz <chmelpe7@fit.cvut.cz>
2013-05-26 18:50:35 +0200

    png decoder - skladani idat chunku
    vyreseno slozeni datovych casti z IDAT chunku
    todo: dekodovat + odfiltrovat na rgb

--- image_png.py
+++ image_png.py
@@ -43,7 +43,6 @@ class PngReader():
 
 		#kontrola hlavicky PNG souboru
 		self.header = self.parser(self.data,0,8)
-		print(self.header)
 		if self.header != b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A':
 			raise PNGWrongHeaderError
 
@@ -52,6 +51,15 @@ class PngReader():
 		if self.ihdr[8:] != b'\x08\x02\x00\x00\x00':
 			raise PNGNotImplementedError
 
+		#vyriznuti vsech IDAT chunku
+		#predpoklada navaznost IDAT chunku
+		#33 = 8 (PNG hlavicka) + 25 (IHDR)
+		#12 = velikost IEND
+		self.idat = self.data[33:-12]
+
+		#slozeni datovych casti vice IDAT chunku
+		self.idat_data = self.IDAT_merge()
+
 	#vyparsovani pozadovane casti a spojeni do binarniho retezce
 	def parser(self,data,start,end):
 		s = b""
@@ -59,12 +67,22 @@ class PngReader():
 			s += data[i]
 		return s
 
+	#spojeni datovych casti vice IDAT chunku
+	def IDAT_merge(self):
+		data = b""
+		start = 8
+		while(start < len(self.idat)):
+			end = start + self.BE_convert(self.idat,start-8) #8 = delka a typ aktualniho
+			data += self.parser(self.idat,start,end)
+			start = end + 12 #12 = 4 (kontrolniho soucet predchoziho) + 8 (delka a typ aktualniho)
+		return data
+
 	#slozeni 4B cisla ulozeneho v BigEndianu
-	def BE_convert(self,start):
-		num = self.data[start] * 16777216
-		num += self.data[start+1] * 65536
-		num += self.data[start+2] * 256
-		num += self.data[start+3]
+	def BE_convert(self,data,start):
+		num = ord(data[start]) * 16777216
+		num += ord(data[start+1]) * 65536
+		num += ord(data[start+2]) * 256
+		num += ord(data[start+3])
 		return num
 
 	#vypis
@@ -77,6 +95,8 @@ class PngReader():
 
 		print("Header: " + str(self.header))
 		print("IHDR: " + str(self.ihdr))
+		print("IDAT: " + str(self.idat))
+		print("IDAT_DATA: " + str(self.idat_data))
 
 
 #--------------------------MAIN--------------------------
