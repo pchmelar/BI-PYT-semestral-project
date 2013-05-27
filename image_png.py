8ddd34beed6929e1fdd0697fd27215113518b56b
filletzz <chmelpe7@fit.cvut.cz>
2013-05-27 20:26:33 +0200

    png decoder - vytvoreni ntic
    nutno dodelat filtraci

--- image_png.py
+++ image_png.py
@@ -16,6 +16,7 @@
 
 import os
 import sys
+import zlib
 from argparse import ArgumentParser
 
 class PNGWrongHeaderError(Exception):
@@ -51,6 +52,10 @@ class PngReader():
 		if self.ihdr[8:] != b'\x08\x02\x00\x00\x00':
 			raise PNGNotImplementedError
 
+		#ziskani vysky/sirky PNG souboru
+		self.width = self.BE_convert(self.data,16)
+		self.height = self.BE_convert(self.data,20)
+
 		#vyriznuti vsech IDAT chunku
 		#predpoklada navaznost IDAT chunku
 		#33 = 8 (PNG hlavicka) + 25 (IHDR)
@@ -59,6 +64,33 @@ class PngReader():
 
 		#slozeni datovych casti vice IDAT chunku
 		self.idat_data = self.IDAT_merge()
+
+		#dekomprese
+		self.idat_decompress = zlib.decompress(self.idat_data)
+
+		#nahrani dekompresovanych dat do seznamu
+		rgb_temp = []
+		for i in range(0,len(self.idat_decompress)):
+			rgb_temp.append(self.idat_decompress[i])
+
+		#ulozeni radkovych filtru
+		self.filters = []
+		delete = []
+		for i in range(0,len(self.idat_decompress),(self.width*3)+1):
+			self.filters.append(rgb_temp[i])
+			delete.append(i)
+
+		#vymazani filtru ze seznamu
+		for i in range(len(delete)-1,-1,-1):
+			del(rgb_temp[delete[i]])
+
+		#utvoreni n-tic a ulozeni do self.rgb
+		rgb_row = []
+		for i in range(0,len(rgb_temp)+1,3):
+			rgb_row.append(tuple(rgb_temp[i:i+3]))
+			if len(rgb_row) == self.width:
+				self.rgb.append(rgb_row)
+				rgb_row = []
 
 	#vyparsovani pozadovane casti a spojeni do binarniho retezce
 	def parser(self,data,start,end):
@@ -70,9 +102,9 @@ class PngReader():
 	#spojeni datovych casti vice IDAT chunku
 	def IDAT_merge(self):
 		data = b""
-		start = 8
+		start = 8 #8 = delka a typ aktualniho
 		while(start < len(self.idat)):
-			end = start + self.BE_convert(self.idat,start-8) #8 = delka a typ aktualniho
+			end = start + self.BE_convert(self.idat,start-8) 
 			data += self.parser(self.idat,start,end)
 			start = end + 12 #12 = 4 (kontrolniho soucet predchoziho) + 8 (delka a typ aktualniho)
 		return data
@@ -85,6 +117,19 @@ class PngReader():
 		num += ord(data[start+3])
 		return num
 
+	#paeth filtr
+	def Paeth(self,a,b,c):
+		p = a + b - c
+		pa = abs(p - a)
+		pb = abs(p - b)
+		pc = abs(p - c)
+		if pa <= pb and pa <= pc:
+			return a
+		elif pb <= pc:
+			return b
+		else:
+			return c 
+
 	#vypis
 	def png_print(self):
 		for i in self.data:
@@ -95,8 +140,13 @@ class PngReader():
 
 		print("Header: " + str(self.header))
 		print("IHDR: " + str(self.ihdr))
+		print("Width: " + str(self.width))
+		print("Height: " + str(self.height))
 		print("IDAT: " + str(self.idat))
 		print("IDAT_DATA: " + str(self.idat_data))
+		print("IDAT_DECOMPRESSED: " + str(self.idat_decompress))
+		print("Row filters: " + str(self.filters))
+		print("RGB pixels: " + str(self.rgb))
 
 
 #--------------------------MAIN--------------------------
